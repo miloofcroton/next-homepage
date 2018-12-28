091af8d6c94e5f56abd22e459d9f0af79872a34d
Jack <ajackofsometrades@gmail.com>
2018-12-28 07:59:00 -0800

    fetching batman data

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/p/hello-nextjs': { page: '/post', query: { title: 'Hello Next.js' } },
+      '/p/learn-nextjs': { page: '/post', query: { title: 'Learn Next.js is awesome' } },
+      '/p/deploy-nextjs': { page: '/post', query: { title: 'Deploy apps with Zeit' } },
+      '/p/exporting-pages': { page: '/post', query: { title: 'Learn to Export HTML Pages' } }
+    }
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
     // Perform customizations to webpack config
     // Important: return the modified config
